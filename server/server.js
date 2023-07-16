d389f033b5cb7440efd429a1e775a4ee184d87e0
manumurillo0430 <manurillo33@gmail.com>
2023-07-16 17:12:33 +0300

    backend

--- server/server.js
+++ server/server.js
@@ -1,18 +1,31 @@
 const express = require("express");
-const cors = require("cors");
-const dalleRoute = require("./Routes/dalleRoute");
+const { Configuration, OpenAIApi } = require("openai");
 const dotenv = require("dotenv");
+const router = express.Router();
 
 dotenv.config();
 
-const app = express();
-app.use(
-  cors({
-    origin: ["http://localhost:8080"],
-    credentials: true,
-  })
-);
-app.use(express.json({ limig: "50mb" }));
-app.use("/dalle_image", dalleRoute);
+const config = new Configuration({
+  apiKey: process.env.OPENAI_KEY,
+});
 
-app.listen(8080, () => console.log("Server has started on port 8080"));
+const openai = new OpenAIApi(config);
+
+router.post("/", async (req, res) => {
+  try {
+    const { prompt } = req.body;
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+    const image = response.data.data[0].b64_json;
+    res.status(200).json({ image: image });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+module.exports = router;
